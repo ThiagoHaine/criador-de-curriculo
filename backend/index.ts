279c32046060f769e91d30bf955812808e0f426b
Thiago Haine <thiago.haine@hotmail.com>
2023-02-17 09:05:37 -0300

    Problema com cors resolvido

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,11 @@
 import express, { Express, Request, Response } from 'express';
 import User from './classes/User';
 import userController from './controllers/userController';
+var cors = require('cors')
 
 const app: Express = express();
 app.use(express.json());
+app.use(cors())
 
 const port = 3001
 
